a1a4a19d4ac59224376d924b174fdbbc881d6327
Pavel <serpork@ya.ru>
2020-10-02 11:56:03 +0700

    Updates

---
+++ precache-manifest.f517e4af2a2446c6d9c1ddd9b838468f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27ab53491e21eead54f2acfe6bec502",
+    "url": "/alina/index.html"
+  },
+  {
+    "revision": "0a511eeb866b6b73d5b7",
+    "url": "/alina/static/css/main.802f8f68.chunk.css"
+  },
+  {
+    "revision": "7de1440cba6866f70dc8",
+    "url": "/alina/static/js/2.0368c16d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alina/static/js/2.0368c16d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a511eeb866b6b73d5b7",
+    "url": "/alina/static/js/main.ab98acb9.chunk.js"
+  },
+  {
+    "revision": "7a3f71871d56b666bd08",
+    "url": "/alina/static/js/runtime-main.afe677da.js"
+  }
+]);
