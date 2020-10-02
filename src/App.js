66dead0c39c24acf2c4294ab60547bd31597934f
Pavel <serpork@ya.ru>
2020-10-02 11:35:33 +0700

    add reset button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Field from './Field'
 import Timer from './Timer'
 import Form from './Form'
@@ -7,9 +7,11 @@ import './App.css';
 function App() {
   const [roundsCount, setRoundsCount] = useState('')
   const [start, setStart] = useState(false)
-
+  const [reset, setReset] = useState(false)
 
   const onStartPress = () => setStart(true)
+
+  const onReset = () => setReset(true)
 
   const onChange = ({target: {value}}) => {
     if(!isNaN(value)) {
@@ -17,16 +19,30 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    reset && setReset(false)
+  }, [reset])
+
+  if(reset) return (
+    <div className="App">
+      <div className={'container'}>
+      </div>
+    </div>
+  )
+
   return (
     <div className="App">
       {start ? (
-        <Form count={Number(roundsCount) || 8} min={1} max={19} />
+        <div className={'container'}>
+          <Form count={Number(roundsCount) || 8} min={1} max={19} />
+          <button className={'submit_btn'} onClick={onReset}>Заново</button>
+        </div>
       ) : (
         <div className={'greeting'}>
           <h2>Привет, Алина!</h2>
           <div className={'fieldItem'}>
             <div className={'label'}>Сколько примеров будем решать?</div>
-            <input className={'roundInput'} onChange={onChange} value={roundsCount}/>
+            <input placeholder={'Как насчет 8?'} className={'roundInput'} onChange={onChange} value={roundsCount}/>
           </div>
           <button className={'submit_btn'} onClick={onStartPress}>Начать</button>
         </div>
