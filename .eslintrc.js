2d261d9f7251e3bebbce559135823e7491627011
Pavel <serpork@ya.ru>
2020-10-10 13:34:08 +0700

    add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    'react-app',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+};
